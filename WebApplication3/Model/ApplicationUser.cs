7041228b3d7760aa20bf8a5dfe04d257b19b0ff7
n0c-turn3 <hyphen.codes@gmail.com>
2024-01-16 10:05:08 +0800

    Add project files.

---
+++ WebApplication3/Model/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApplication3.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string CreditCard { get; set; } = string.Empty;
+    }
+}
